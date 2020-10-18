280752c04ec852d99a97b4c76e33e864154a52ba
Sumaya Hassan <sumaya_198@hotmail.co.uk>
2020-10-18 12:33:59 +0100

    fifth commit

--- script/script.js
+++ script/script.js
@@ -1,4 +1,9 @@
+//display current day and time at the top of the page
+
+$("#currentDay").text(moment().format("MMMM DD, YYYY HH:mA"));
+
 //save events to local storage
+
 $(".saveBtn").on("click", function(){
 
     var value = $(this).siblings(".description").val();
@@ -7,29 +12,10 @@ $(".saveBtn").on("click", function(){
 
 });
 
-var interval = setInterval(hourUpdater, 15000);
-// Section to display the times on the screen in loc 
-// here we need to access the values from localstorage and GET the times to display them on screen
-$("#hour-9").val(localStorage.getItem("hour-9"));
-$("#hour-10").val(localStorage.getItem("hour-10"));
-$("#hour-11").val(localStorage.getItem("hour-11"));
-$("#hour-12").val(localStorage.getItem("hour-12"));
-$("#hour-13").val(localStorage.getItem("hour-13"));
-$("#hour-14").val(localStorage.getItem("hour-14"));
-$("#hour-15").val(localStorage.getItem("hour-15"));
-$("#hour-16").val(localStorage.getItem("hour-16"));
-$("#hour-17").val(localStorage.getItem("hour-17"));
-$("#hour-18").val(localStorage.getItem("hour-18"));
-
-$("#currentDay").text(moment().format("MMMM DD, YYYY"));
-
-
 
 // Function to update the color of the time display
 function hourUpdater() {
     var currentTime = moment().hours();
-    
-    // pull the id from each id within the time column
 
  $(".time-block").each(function(){
      var currentHour = parseInt($(this).attr("id").split("-")[1]);
@@ -45,15 +31,29 @@ function hourUpdater() {
         }else {
             $(this).addClass('future');
         }
- })
-    
-        
-   
+ }) 
 
-}
-
+};
 
 hourUpdater()
+///get items from local storage and display on UI for each event
+$("#hour-9 .description").val(localStorage.getItem("hour-9"));
+$("#hour-10 .description").val(localStorage.getItem("hour-10"));
+$("#hour-11 .description").val(localStorage.getItem("hour-11"));
+$("#hour-12 .description").val(localStorage.getItem("hour-12"));
+$("#hour-13 .description").val(localStorage.getItem("hour-13"));
+$("#hour-14 .description").val(localStorage.getItem("hour-14"));
+$("#hour-15 .description").val(localStorage.getItem("hour-15"));
+$("#hour-16 .description").val(localStorage.getItem("hour-16"));
+$("#hour-17 .description").val(localStorage.getItem("hour-17"));
+$("#hour-18 .description").val(localStorage.getItem("hour-18"));
+
+
+
+
+
+
+
 
 
 
